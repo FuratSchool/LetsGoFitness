bf4cb110c77badf303f3fb9a0fc1716ad17e0299
FuratSchool <36156390+FuratSchool@users.noreply.github.com>
2024-12-03 01:50:37 +0100

    Initial Commit

---
+++ LetsGoFitnessTests/LetsGoFitnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  LetsGoFitnessTests.swift
+//  LetsGoFitnessTests
+//
+//  Created by Furat Dahsh on 03/12/2024.
+//
+
+import Testing
+@testable import LetsGoFitness
+
+struct LetsGoFitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
